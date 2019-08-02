ce17d10a4a96bef347cc871573afe080259e0b33
Jonathan <atta.jon.t@gmail.com>
2019-08-02 14:09:49 +0200

    Loan function and player hud  added

--- Code.js
+++ Code.js
@@ -6,31 +6,43 @@ let Cow = class Cow {
 
 }
 
-let baseMoney = 9500;
+let Loan = class Loan{
+    constructor(amount, monthToPay){
+        this.amount=amount;
+        this.monthToPay = monthToPay;
+        let mensuality = amount/100*121;
+    }
+
+}
+
+// player = le joueur
+// JustRuledPlayer = joueur avec regle juste de production
+
+let baseMoneyPlayer = 9500;
+let baseMoneyJustRuledPlayer = 9500;
+
+let creditPlayer = [];
+let creditJustRuledPlayer = [];
 
 let cowCost = 1435;
 let groundCost = 6000;
 
 let herd = []; // troupeau 
 
-
 function Error(string){
     document.getElementById('divError').innerHTML= string;
 }
 
+function PlayerHud(){ // function qui affichera les information du joueur
+    document.getElementById("gameInformation").innerHTML = `<progress id="Argent joueur sur argent joueur possible" max="${baseMoneyJustRuledPlayer}" value="${baseMoneyPlayer}"> ${baseMoneyPlayer} / ${baseMoneyJustRuledPlayer}" </progress>`;
+}
 
 document.getElementById("buyCowButton").addEventListener("click",() => {
-
-    // si on a assez d'argent on ajoute une vache et on enleve le prix de la vache
-    // sinon, on affiche un message d'erreur du style = "too poor to buy a cow, make a credit or wait for benefit."
-    cowCost<baseMoney? herd.push(new Cow(0)) : error("too poor to buy a cow, make a credit or wait for benefit.");
+    cowCost<=baseMoneyPlayer? herd.push(new Cow(0)) : error("too poor to buy a cow, make a credit or wait for benefit.");
 });
 
 document.getElementById("buyGroundButton").addEventListener("click",() => {
-
-    // si on a assez d'argent on ajoute une vache et on enleve le prix de la vache
-    // sinon, on affiche un message d'erreur du style = "too poor to buy a cow, make a credit or wait for benefit."
-    cowCost<baseMoney? herd.push(new Cow(0)) : error("too poor to buy a ground, make a credit or wait for benefit.");
+    groundCost<=baseMoneyPlayer? herd.push(new Cow(0)) : error("too poor to buy a ground, make a credit or wait for benefit.");
 });
 
 
