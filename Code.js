50fae92aa7d59127f2d62240be11c64cbfe5e70c
Jonathan <atta.jon.t@gmail.com>
2019-08-02 14:42:21 +0200

    production, index and first prototype

--- Code.js
+++ Code.js
@@ -1,4 +1,5 @@
-let Cow = class Cow {
+(() => {
+    let Cow = class Cow {
     constructor(){
         let spanLife= 7300;
         let currentLifeTime=0;
@@ -24,26 +25,74 @@ let baseMoneyJustRuledPlayer = 9500;
 let creditPlayer = [];
 let creditJustRuledPlayer = [];
 
+let MilkPricePlayer = 0.40;
+let MilkProductionPricePlayer = 0.27;
+
+
 let cowCost = 1435;
 let groundCost = 6000;
 
 let herd = []; // troupeau 
+let ground = 0; // number of ground
+
+let milkStocked = 0;
 
 function Error(string){
     document.getElementById('divError').innerHTML= string;
 }
 
 function PlayerHud(){ // function qui affichera les information du joueur
-    document.getElementById("gameInformation").innerHTML = `<progress id="Argent joueur sur argent joueur possible" max="${baseMoneyJustRuledPlayer}" value="${baseMoneyPlayer}"> ${baseMoneyPlayer} / ${baseMoneyJustRuledPlayer}" </progress>`;
+    document.getElementById("gameInformation").innerHTML = 
+    `
+    ${baseMoneyPlayer} / ${baseMoneyJustRuledPlayer}
+    <progress id="ProgressPlayer" max="${baseMoneyJustRuledPlayer}" value="${baseMoneyPlayer}"> ${baseMoneyPlayer} / ${baseMoneyJustRuledPlayer} </progress>
+    Milk in stock ${milkStocked}
+    `;
+}
+
+function production()
+{
+    milkStocked += herd.length;
+    baseMoneyPlayer -= herd.length*MilkProductionPricePlayer;
+
 }
 
 document.getElementById("buyCowButton").addEventListener("click",() => {
-    cowCost<=baseMoneyPlayer? herd.push(new Cow(0)) : error("too poor to buy a cow, make a credit or wait for benefit.");
+    if(cowCost<=baseMoneyPlayer)
+    {
+        herd.push(new Cow(0));
+        baseMoneyPlayer -= cowCost;
+    } 
+    else
+    {
+        Error("too poor to buy a cow, make a credit or wait for benefit.");
+    }
 });
 
 document.getElementById("buyGroundButton").addEventListener("click",() => {
-    groundCost<=baseMoneyPlayer? herd.push(new Cow(0)) : error("too poor to buy a ground, make a credit or wait for benefit.");
+    if(groundCost<=baseMoneyPlayer)
+    {
+        ground++;
+        baseMoneyPlayer -= groundCost;
+    } 
+    else
+    {
+        Error("too poor to buy a ground, make a credit or wait for benefit.");
+    }
 });
 
+document.getElementById("sellMilk").addEventListener("click",() => {
+    baseMoneyPlayer+= milkStocked*MilkProductionPricePlayer;
+    milkStocked = 0;
+});
 
+function main(){
+    PlayerHud();
+    production();
+    
+}
+
+setInterval(main,60);
+
+})();
 
